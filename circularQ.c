4cc3784a17d2d6361515e87dff80f1e308ec310e
basil <basilpaul230@gmail.com>
2024-12-09 10:21:32 +0530

    set bit string

---
+++ circularQ.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#define size 100
+int array[size],n;
+void display();
+void createarray();
+void insertion();
+void deletion();
+int search();
+int bubblesort();
+void reversearray();
+void main()
+{
+    createarray();
+    while(1)
+    {
+        int choice;
+        printf("enter 1 to create,2 to insert,enter 3 to delete, 4 to display, 5 to search, 6 to sort,7 to reverse,8 to exit");
+        scanf("%d",&choice);
+        swi
+    }
+}
